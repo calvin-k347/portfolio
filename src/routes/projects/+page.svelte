9b61465b781865ab8776d4e1ad48f3b09a6b0b61
calvin-k347 <clark.calvink@gmail.com>
2025-08-27 23:17:45 -0400

    snippets for projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,45 +1,34 @@
 <script>
   import cordele from '$lib/assets/cordele.jpg';
   import minionese from '$lib/assets/Minionese.png';
+  import langandnames from '$lib/assets/langandnames.png'
 </script>
 <div class="flex">
-    <h1 class="mx-auto text-2xl">Highlighted projects have full pages, click to learn more!</h1>
+    <h1 class="mx-auto text-2xl">Projects have full pages, click to learn more!</h1>
 </div>
 <div class="grid grid-cols-1 gap-8 mx-auto mt-20">
-    <div class="rounded-lg grid grid-cols-1 md:flex gap-5 mx-auto">
-        <div class="grid-cols-1 gap-y-4 border-4 rounded-lg border-[#6ac8c8] hover:cursor-pointer hover:shadow-xl bg-white w-72 h-72">
-            <img class="rounded-lg border-b-2" src={cordele} alt="welcome to cordele sign">
-            <p class="justify-center px-2 font-bold">Language Change and Migration in Cordele, GA</p>
+    {#snippet project(caption, image, description, link)}
+        <div class="rounded-lg grid grid-cols-1 md:flex gap-5 mx-auto">
+        <div class="grid-cols-1 gap-y-4 border-2 rounded-lg border-[#6ac8c8]  shadow-lg hover:shadow-[#6ac8c8] transition-all ease-in bg-white w-72 h-72">
+            {#if link}
+            <a href="/projects/{link}}">
+            
+            <img class="rounded-lg min-h-32" src={image} alt="welcome to cordele sign">
+            <p class="justify-center px-2 font-bold">{caption}</p>
+            </a>
+            {:else}
+            <img class="rounded-lg" src={image} alt="welcome to cordele sign">
+            <p class="justify-center px-2 font-bold whitespace-pre-line">{caption}</p>
+            {/if}
         </div>
         <div class="bg-[#d6f0f0] p-4 min-w-72 max-w-72 rounded-xl">
-            <p>I was born in Cordele and most of my family is from there. <br> <br>
-                We moved when I was 6, and over the years I've noticed more and more families move away from the town. <br> <br> Why? What consequneces does this have on language change?  </p>
+            <p>{@html description.replaceAll('\n', '<br>')}</p>
         </div>
-    </div>
-
-    <div class="rounded-lg grid grid-cols-1 md:flex gap-5 mx-auto">
-        <div class="grid-cols-1 gap-y-4 border-4 rounded-lg border-[#6ac8c8] hover:cursor-pointer hover:border-blue-500 bg-white w-72 h-72">
-            <img class="rounded-lg" src={minionese} alt="welcome to cordele sign">
-            <p class="justify-center px-2 font-bold">Minionese Speech Processing</p>
         </div>
-        <div class="bg-[#d6f0f0] p-4 min-w-72 max-w-72 rounded-xl ">
-            <p>Speech recogniton tools require an orthographic transcription of the language. What can we do for unwritten languages?
-                <br><br> 
-                The minions speak an unwritten language, so I developed a pipline to process their speech and reconstruct phonological class
-            </p>
-        </div>
-    </div>
-
-    <div class="rounded-lg grid grid-cols-1 md:flex gap-5 mx-auto">
-        <div class="grid-cols-1 gap-y-4 border-4 rounded-lg border-blue-700 hover:cursor-pointer hover:border-blue-500 bg-gray-200/50 w-72 h-72">
-            <img class="rounded-lg" src={cordele} alt="welcome to cordele sign">
-            <p class="justify-center px-2 font-bold">Language Change and Migration in Cordele, GA</p>
-        </div>
-        <div class="bg-gray-200/50 p-4 min-w-72 max-w-72 rounded-xl">
-            <p>I was born in Cordele and most of my family is from there. <br> <br>
-                We moved when I was 6, and over the years I've noticed more and more families move away from the town. <br> <br> Why? What consequneces does this have on language change?  </p>
-        </div>
-    </div>
-
-    <div class="rounded-lg border-2 bg-gray-200 w-72 h-72 mx-auto"></div>
+    {/snippet}
+    {@render project("Language Change and Migration in Cordele, GA", cordele, "I was born in Cordele and most of my family is from there. \n\n We moved when I was 6, and over the years I've noticed more and more families move away from the town. \n \n Why? What consequneces does this have on language change?", "/projects/cordele")}
+    {@render project("Minionese Speech Processing", minionese, "Speech recogniton tools require an orthographic transcription of the language. What can we do for unwritten languages? \n\n The minions speak an unwritten language, so I developed a pipline to process their speech and reconstruct phonological class" )}
+    {@render project("Langauge and Names", langandnames, "Fun simulation to see how your name may changed over the next 100 years using a Generalized Addiative Model created on decades of social security baby name data! \n\n Based on the paper \"Modeling Language Change in English First Names\" (Williams, Savannah Jane, and Margaret Renwick. 2022)" )}
+    
+    <div class="rounded-lg border-2 bg-gray-200 w-72 h-72 mx-auto"><p class="text-2xl m-auto mt-24 text-center">More soon!</p></div>
 </div>
---
+++ src/routes/projects/cordele/+page.svelte
@@ -0,0 +1,15 @@
+<div class="grid gap-8 ">
+    <div class="mt-8 mx-auto grid full md:w-2/3 py-4">
+        <h1 class="mx-auto text-2xl font-bold">Cordele Voices</h1>
+        <p class="mx-auto text-xl italic underline">Southern Georgia Sociophonetics. Urban movement as a catalyst for language change</p>
+    </div>
+    {#snippet blog(date,title, text, img)}
+        <div class="grid mx-auto bg-white rounded-xl w-full md:w-2/3 py-4 border-2 border-[#6ac8c8]">
+            <p class="text-xl text-[#6ac8c8] ml-4 text-">{date}: <span class="text-black"> {title}</span></p>
+            <p class="px-4 ">{text}</p>
+        </div>
+    {/snippet}
+    {@render blog("8/24", "King Papers", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", [])}
+    {@render blog("Summer 2025","Project Ideation", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",[])}
+
+</div>
