c830f3b81f1ad1d9cb08046667cf63c884e296e5
calvin-k347 <clark.calvink@gmail.com>
2025-08-28 14:15:26 -0400

    name project infra

---
+++ src/routes/projects/names/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Block from "./Block.svelte";
+</script>
+<div class="grid">
+    <div id="app" class="grid grid-cols-2 gap-8 m-auto mt-24">
+        <form class="m-auto grid gap-4 text-xl">
+            <label for="name">Enter you name:</label>
+        <input type="text" id="username" name="username" placeholder="......" maxlength="50" size="30">
+        <input type="submit" value="Submit" class="bg-white outline-[] rounded-lg">
+        </form>
+        <div class="flex m-auto">
+            <div class="w-10 h-10 border-2 bg-white"></div>
+            <Block message="hi"></Block>
+        </div>
+    </div>
+    
+</div>
