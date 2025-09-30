73636ed13043f375f3eda87bbe2d606a5ac1253c
cubedhuang <99danielh@gmail.com>
2025-09-30 12:16:59 -0400

    design help

---
+++ src/routes/projects/Project.svelte
@@ -0,0 +1,22 @@
+<script>
+    const {
+        caption,
+        image,
+        link,
+        children
+    } = $props();
+</script>
+
+<div class="mx-auto grid grid-cols-1 gap-2 rounded-lg sm:grid-cols-3">
+	<svelte:element
+		this={link ? 'a' : 'div'}
+		href={link}
+		class="rounded-lg border-2 border-blue-300 bg-white shadow-lg transition-all hover:shadow-blue-300"
+	>
+		<img class="min-h-32 rounded-t-lg" src={image} alt="welcome to cordele sign" />
+		<p class="justify-center p-3 font-bold">{caption}</p>
+	</svelte:element>
+	<div class="col-span-2 rounded-xl bg-blue-100 space-y-4 p-4">
+		{@render children()}
+	</div>
+</div>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -7,36 +7,45 @@
 
     })
 </script>
-<div class="grid-cols-1 mx-auto rounded-lg w-full md:w-1/2 p-8 text-lg">
+
+<div class="grid-cols-1 mx-auto rounded-lg text-lg">
     <div class=" bg-white flex flex-col rounded-lg border-2 border-[#6ac8c8] p-4 shadow-lg mb-8">
         <h1 class="text-2xl italic font-bold ">Education</h1>
         <p class="font-bold">Georgia Institute of Technology</p>
         <p class="text-[#6ac8c8] font-bold">2024-2028</p>
-        <li class="ml-2">Bachelor of Science in Computer Science and Minor in Linguistics</li>
-        <li  class="ml-2">Concentrations: Theoretical CS and Human Centered Computing</li>
-        <li  class="ml-2">GPA: 3.60</li>
+        <ul class="list-disc ml-6">
+            <li>Bachelor of Science in Computer Science and Minor in Linguistics</li>
+            <li>Concentrations: Theoretical CS and Human Centered Computing</li>
+            <li>GPA: 3.60</li>
+        </ul>
     </div>
 
     <div class="  bg-white flex flex-col rounded-lg border-2 border-[#6ac8c8] p-4 shadow-lg mb-8">
         <h1 class=" italic font-bold text-2xl">Experience</h1>
         <p class="font-bold">Verticaly Integrated Project: Langauge and Idenity in the New South </p>
         <p class="text-[#6ac8c8] font-bold">2025</p>
-        <li class="ml-2">Joined the team this semester!</li>
-        <li class="ml-2">Sociolinguistic interviewing, transcription, and data analysis using Python and R</li>
-        <li class="ml-2">Investigating 2nd wave sociolinguistic variation among Georgia Tech Students that grew up in Georgia</li>
+        <ul class="list-disc ml-6">
+            <li>Joined the team this semester!</li>
+            <li>Sociolinguistic interviewing, transcription, and data analysis using Python and R</li>
+            <li>Investigating 2nd wave sociolinguistic variation among Georgia Tech Students that grew up in Georgia</li>
+        </ul>
         <p class="font-bold">Robins Airforce Base: Software Engineering Intern</p>
         <p class="text-[#6ac8c8] font-bold">2025</p>
-        <li class="ml-2">Modeled and constructed circuits to simulate systems control for aircraft flare pattern dispense tests using LEDs</li>
-        <li class="ml-2">Developed a Smart Car with Raspberry Pi and various environmental sensors to navigate courses</li>
-        <li class="ml-2">Designed YOLOv5-based vision model to give car expanded navigation capabilities </li>
+        <ul class="list-disc ml-6">
+            <li>Modeled and constructed circuits to simulate systems control for aircraft flare pattern dispense tests using LEDs</li>
+            <li>Developed a Smart Car with Raspberry Pi and various environmental sensors to navigate courses</li>
+            <li>Designed YOLOv5-based vision model to give car expanded navigation capabilities </li>
+        </ul>
     </div>
 
     <div class="  bg-white flex flex-col rounded-lg border-2 border-[#6ac8c8] p-4 shadow-lg mb-8">
         <h1 class=" italic font-bold text-2xl">Awards / Honors</h1>
         <p class="font-bold">Oral Presentation at Emory Undergraduate Lingustics Conference 6</p>
         <p class="text-[#6ac8c8] font-bold">2025</p>
-        <li class="ml-2">Evaluated phonological structure in low-resource speech using Minionese as a case study</li>
-        <li class="ml-2"> Used output from Allosaurus phone recognizer model in an PCA + k-means algorithm for phonological discovery to hierarchically organize phonological class in nonsense language</li>
+        <ul class="list-disc ml-6">
+            <li>Evaluated phonological structure in low-resource speech using Minionese as a case study</li>
+            <li> Used output from Allosaurus phone recognizer model in an PCA + k-means algorithm for phonological discovery to hierarchically organize phonological class in nonsense language</li>
+        </ul>
         <p class="font-bold">Dean's List</p>
         <p class="text-[#6ac8c8]font-bold">2024, 2025</p>
     </div>
