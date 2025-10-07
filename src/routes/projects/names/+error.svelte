c39a8219d9ba0ed80f25fac7f0b7e3d83a6d3632
calvin-k347 <clark.calvink@gmail.com>
2025-10-07 07:18:30 -0400

    add error message for names project

---
+++ src/routes/projects/names/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import rarename from "$lib/assets/rarenamelocal.png"
+</script>
+<div class="p-6 rounded-lg grid gap-4 bg-white border-2 border-blue-300">
+    <p>Sorry, this name cannot be processed :(</p>
+    <p>Locally, names like this one would be handeled with g2p-en, which is a grapheme to phoneme model, but there is an issue with deployment</p>
+    <p>Refresh and try a new name</p>
+</div>
+
+
+<img src="{rarename}" class="mb-6"  alt="rare name working on local server">
+<div class="p-6 rounded-lg bg-white border-2 border-blue-300">
+    <p>Here is an image of a rarer name, "Calvon", being processed when the program is ran locally</p>
+</div>
